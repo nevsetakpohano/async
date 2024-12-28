1ce235a984f1a8ab87f94caba4acd38ef7c42d3b
nevsetakpohano <aya101318@gmail.com>
2024-12-28 20:29:36 +0200

    Wrote use cases for the promise based solution

--- index2.js
+++ index2.js
@@ -2,16 +2,22 @@ function promiseMap(array, promiseCallback) {
     return Promise.all(array.map((item, index) => promiseCallback(item, index, array)));
 }
 
-function examplePromiseCallback(item, index) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(`Processed ${item} at index ${index}`);
-        }, Math.random() * 1000);
-    });
-}
+const slowCallback = (item) => {
+    return new Promise((resolve) => setTimeout(() => resolve(`Slow Process ${item}`), 1000));
+};
+
+const fastCallback = (item) => {
+    return new Promise((resolve) => setTimeout(() => resolve(`Fast Process ${item}`), 100));
+};
 
 const inputArray = [1, 2, 3, 4, 5];
-promiseMap(inputArray, examplePromiseCallback)
+
+promiseMap(inputArray, slowCallback)
     .then((results) => {
-        console.log("Promise-based Results:", results);
+        console.log("Slow Callback Results:", results);
+    });
+
+promiseMap(inputArray, fastCallback)
+    .then((results) => {
+        console.log("Fast Callback Results:", results);
     });
